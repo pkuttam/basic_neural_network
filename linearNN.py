f9696f290fa4b2e9e7df4b2f189a7df8df99f7e1
Prakash Uttam <pk.uttam@gmail.com>
2017-02-22 11:47:32 +0530

    Update linearNN.py

--- linearNN.py
+++ linearNN.py
@@ -1,3 +1,25 @@
+# maintained by Prakash K Uttam
+
+# W = [ [W11, W12, W13, ......], [W21, W22, W23, ......], .........]
+# dW = d(outLayer)/d(Weight)
+# dI = d(outLayer)/d(inputLayer)
+# dB = d(outputLayer)/d(inputLayer)
+# I = inputLayer
+# O = output
+# B = biased
+
+# since all the weight matrix,biased and input are only related to individual output.So,there won't be addition of two differential
+# in dW and dI
+
+# I1W11 + I2W21 + I3W31 + ..... = O1
+# I1W12 + I2W22 + I3W32 + ...... = O2
+# ..................................
+
+# dW = [ [ dO1/dW11, dO2/dW12, dO3/dW13, ........], [dO1/dW21, dO2/dW22, dO3/dW23, ......], ..........]
+# dW = [ [ x1, x1, x1 .....], [x2, x2, x2, .....], ..........]]
+
+# dI = [ [ dO1/dI1, dO2/dII, dO3/dI1, ......] , [dO1/dI2, dO2/dI2,dO3/dI3, ......],.....] = W
+
 import numpy as np
 
 class linearNN:
